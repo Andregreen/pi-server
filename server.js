cb49492408e43d7eae8267545546a8cb9047db0d
Zachary <zachary@MacBook-Pro-de-Zachary.local>
2017-11-16 18:05:24 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,69 @@
+///////////////////////////////////////Imports
+const Firebase=require('firebase');
+const SerialPort=require('serialport');
+const Xbee=require('xbee-api');
+const readline=require('readline');
+const {StringDecoder}=require('string_decoder');
+///////////////////////////////////////
+
+///////////////////////////////////////Config and Varibles
+var config = {
+    apiKey: "AIzaSyDIQMP6yMBrKuwDvtRaSmhwYMMZC3FyqpY",
+    authDomain: "smart-garden-ca02a.firebaseapp.com",
+    databaseURL: "https://smart-garden-ca02a.firebaseio.com",
+    projectId: "smart-garden-ca02a",
+    storageBucket: "smart-garden-ca02a.appspot.com",
+    messagingSenderId: "269607227081"
+  };
+Firebase.initializeApp(config);
+
+const serialport=new SerialPort('/dev/tty.usbserial-A51F2158',{baudRate: 9600});
+//const Readline = serialport.parsers.readline;
+const userID='DJvMhRfvALc3SyUzbQeCdNGVNcl2';// set this equal to something that changes with each xbee
+const database=Firebase.database();
+const decoder = new StringDecoder('utf8');
+
+///////////////////////////////////////
+
+///////////////////////////////////////Functions and Console.logs
+serialport.on('readable',function(chunk) {
+  //console.log('Data', serialport.read([8]));
+  let date=new Date();
+  let data=serialport.read([8]);
+  console.log('Data', data);
+  let unqId;
+  let value;
+  if (data==null){
+    unqId=null;
+    value=null;
+  }
+  else {
+    unqId=data.slice(0,5).toString();
+    value=data.slice(6,8).toString();
+    var unqIdA = new Buffer(unqId, 'hex');
+    var valueA = new Buffer(value, 'hex');
+    database.ref('Users/'+ userID + '/GardenZones/My Garden/Sensors/'+unqId).update({
+      data: value,
+      icon: 'opacity',
+      id: unqId,
+      title: 'Moisture Sensor 1',
+      type: 'moisture',
+});
+    database.ref('Users/'+ userID +'/History/'+ unqId+"/"+date).set({
+      data:value
+    })
+  }
+  console.log('Unique id:',unqId);
+  console.log('Value:',value);
+
+  //buffer.slice(0, 3);
+  //var textChunk = decoder.write(chunk);
+  
+  //console.log(textChunk)
+  // process utf8 text chunk
+});
+
+
+///////////////////////////////////////
+
+
